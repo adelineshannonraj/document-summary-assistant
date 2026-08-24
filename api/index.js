406e0a1f71534dbcc965d52ebc4699d6defa3d8f
adelineshannonraj <adelineshannonraj@gmail.com>
2026-08-24 21:58:10 +0530

    Add Vercel startup diagnostics

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
-const app = require('../server');
+let app;
+
+try {
+  app = require('../server');
+  console.log('SERVER LOADED SUCCESSFULLY');
+} catch (error) {
+  console.error('SERVER STARTUP ERROR:', error);
+  throw error;
+}
 
 module.exports = app;
